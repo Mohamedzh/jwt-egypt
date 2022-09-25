f75ed642f8936e842cc7c8f84bb2fed59cb50149
MariamYoussef23 <mariamyoussef@aucegypt.edu>
2022-09-25 21:41:43 +0200

    first draft of contact Us UI

---
+++ pages/contact2.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Example from '../components/contactForm'
+
+
+
+type Props = {}
+
+function ContactTest2({ }: Props) {
+    return (
+        <div>
+            <Example />
+        </div>
+    )
+}
+
+export default ContactTest2
