a555d97fc67d24f34c223501c005648b3b40d511
Mohamed Zahran <mohamed.zahran1@live.com>
2022-09-25 16:41:31 +0200

    initial commit

---
+++ pages/test.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import WomenList from '../components/womenList'
+
+type Props = {}
+
+function Test({ }: Props) {
+    return (
+        <div>
+            <WomenList />
+        </div>
+    )
+}
+
+export default Test
