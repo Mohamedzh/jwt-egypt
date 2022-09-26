9abffad9e6f1e6f4470f28a095fd4790bc3c96ac
IbrahimGhorab <ibrahimabdallah81@gmail.com>
2022-09-26 11:02:12 +0200

    hero page

--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,15 @@ import Layout from '../components/layout'
 import { getAllPostsForHome } from '../lib/api'
 import Head from 'next/head'
 import { CMS_NAME } from '../lib/constants'
+import NavigationBar from '../components/NavigatoinBar'
+import HeroPage from '../components/HeroPage'
 
 export default function Index({ allPosts, preview }) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
+  // const heroPost = allPosts[0]
+  // const morePosts = allPosts.slice(1)
   return (
     <>
-      <Layout preview={preview}>
+      {/* <Layout preview={preview}>
         <Head>
           <title>Next.js Blog Example with {CMS_NAME}</title>
         </Head>
@@ -30,15 +32,18 @@ export default function Index({ allPosts, preview }) {
           )}
           {morePosts.length > 0 && <MoreStories posts={morePosts} />}
         </Container>
-      </Layout>
+      </Layout> */}
+
+      <NavigationBar />
+      <HeroPage />
     </>
   )
 }
 
-export async function getStaticProps({ preview = false }) {
-  const allPosts = await getAllPostsForHome(preview)
-  return {
-    props: { allPosts, preview },
-    revalidate: 1,
-  }
-}
+// export async function getStaticProps({ preview = false }) {
+//   const allPosts = await getAllPostsForHome(preview)
+//   return {
+//     props: { allPosts, preview },
+//     revalidate: 1,
+//   }
+// }
