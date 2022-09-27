12528f73948d7a93bd6ec1fe4347e9ee3353b811
IbrahimGhorab <ibrahimabdallah81@gmail.com>
2022-09-27 19:30:36 +0200

    add story schema

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -12,6 +12,7 @@ import author from './author'
 import comment from './comment'
 import person from './women'
 import quotes from './quotes'
+import story from './story'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -23,6 +24,7 @@ export default createSchema({
     // The following are document types which will appear
     // in the studio.
     post,
+    story,
     author,
     category,
     person,
