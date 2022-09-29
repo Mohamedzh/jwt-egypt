171ce664dc5a6d1d4d5d9319ca20b3fcb7d7ae3b
Mohamed Zahran <mohamed.zahran1@live.com>
2022-09-30 00:23:01 +0200

    UI refactoring

--- components/womenListSlide.tsx
+++ components/womenListSlide.tsx
@@ -1,47 +1,61 @@
+import Link from 'next/link'
 import React from 'react'
-import { ImQuotesLeft, ImQuotesRight } from 'react-icons/im'
+import { ImArrowRight, ImQuotesLeft, ImQuotesRight } from 'react-icons/im'
 import { WomenListType } from '../types '
 
 
 function WomenListSlide({ data }) {
     return (
-        <div style={{ backgroundColor: "#e2e2d0"}} className='m-5'>
+        <div style={{ backgroundColor: `${data.themeColors[0].quotesSectionColor.color_code}` }} className='pt-20'>
+
             <div className="carousel w-full">
                 {data.quoteList.map((quote, idx) =>
-                    <div key={idx} id={`slide ${idx}`} className="carousel-item relative w-full">
-                        <div className='rounded-lg shadow-xl mr-5'>
-                            <div className=" grid grid-cols-6 place-items-center h-3/4">
-                                <div className='col-span-2 flex flex-col items-center px-24 py-10'>
-                                    <img className="aspect-auto w-72" src={quote.person.imageUrl} />
-                                    <h2 className="card-title my-5 text-4xl italic">{quote.person.name}</h2>
-                                    <p className=" text-xl italic">{quote.person.job_title}</p>
-                                    <p className=" text-xl italic">{quote.person.department.title}</p>
-                                </div>
-                                <div className='col-span-4 flex flex-col w-4/6'>
-                                    <ImQuotesLeft className='h-5 w-5' />
-                                    <p className='p-1 text-3xl text-justify	'>
-                                        {quote.body}
-                                    </p>
-                                    <ImQuotesRight className='place-self-end h-5 w-5' />
+                    <div key={idx} className='carousel-item relative w-full flex flex-col'>
+                        <h1 id={`slide ${idx}`} className="mb-5 text-5xl font-bold text-gray-900 sm:text-center">
+                            Meet our WunderWomen
+                        </h1>
+                        <div className="border-gray-500 border-2 border-dashed">
+                            <div className='rounded-lg mr-5'>
+                                <div className=" grid grid-cols-6 place-items-center h-3/4">
+                                    <div className='col-span-2 flex flex-col items-center px-24 py-10'>
+                                        <img className="aspect-auto w-72" src={quote.person.imageUrl} />
+                                        <p className="my-5 text-4xl italic">{quote.person.name}</p>
+                                        <p className=" text-xl italic">{quote.person.job_title}</p>
+                                        <p className="text-xl italic">{quote.person.department.title}</p>
+                                    </div>
+                                    <div className='col-span-4 flex flex-col w-4/6'>
+                                        <ImQuotesLeft className='h-5 w-5' />
+                                        <p className='p-1 text-3xl text-center	'>
+                                            {quote.body}
+                                        </p>
+                                        <ImQuotesRight className='place-self-end h-5 w-5' />
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                            <a
-                                href={`#slide ${(idx === 0 ? data.quoteList.length - 1 : idx - 1)}`}
-                                className="btn btn-circle">
-                                ❮
-                            </a>
+                            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                                <a
+                                    href={`#slide ${(idx === 0 ? data.quoteList.length - 1 : idx - 1)}`}
+                                    className="btn btn-circle">
+                                    ❮
+                                </a>
 
-                            <a
-                                href={`#slide ${(idx === data.quoteList.length - 1 ? 0 : idx + 1)}`}
-                                className="btn btn-circle mr-5">
-                                ❯
-                            </a>
+                                <a
+                                    href={`#slide ${(idx === data.quoteList.length - 1 ? 0 : idx + 1)}`}
+                                    className="btn btn-circle mr-5">
+                                    ❯
+                                </a>
+                            </div>
                         </div>
                     </div>
                 )}
             </div>
+            <Link href="/quoteslist">
+                <a >
+                    <p className="text-2xl mx-20 text-wtBlue font-bold flex">
+                        See the whole team <ImArrowRight className="mt-1.5 ml-2" />
+                    </p>
+                </a>
+            </Link>
         </div>
     )
 }
--- pages/careers/index.tsx
+++ pages/careers/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import NavigationBar from '../../components/NavigationBar'
 import { VacancyType } from '../../types '
-import client, { getClient, previewClient } from '../../lib/sanity'
+import { getClient } from '../../lib/sanity'
 import * as Yup from 'yup'
 import 'yup-phone'
 import { useFormik } from 'formik'
@@ -55,7 +55,7 @@ const formik = useFormik({
     // message: Yup.string(),
   }),
   onSubmit: (values) => {
-    // console.log(values)
+    console.log(values)
     //api call
     // formik.resetForm
     //navigate to top or show a pop up message
