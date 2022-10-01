1d54ca1cdc95db76980b7325a7b464607a91887e
Mohamed Zahran <mohamed.zahran1@live.com>
2022-10-01 22:02:26 +0200

    added scrolling

--- studio/schemas/navbarControl.ts
+++ studio/schemas/navbarControl.ts
@@ -5,19 +5,20 @@ export default {
     type: 'document',
     title: 'Navbar',
     icon: BsCardHeading,
+    __experimental_actions: [/*'create',*/ 'update', /*'delete',*/ 'publish'],
     fields: [
         {
             name: 'buttonText',
             title: 'Button Text',
             type: 'string',
-            description: 'The navbar button text',
+            description: 'The navbar button text (Edit-only field).',
             validation: rule => rule.required()
         },
         {
             name: 'logo',
             title: 'Logo',
             type: 'image',
-            description: "The logo image. To change it. click on the 3-dots menu on the right and choose'upload'",
+            description: "The logo image (Edit-only field). To change it. click on the 3-dots menu on the right and choose'upload'",
             options: {
                 hotspot: true,
             },
@@ -41,7 +42,8 @@ export default {
             title: 'Menu text color',
             type: 'reference',
             to: [{ type: 'color' }],
-            description: 'The navbar menu text color',
+            description: 'The navbar menu text color (Edit-only field).',
+            validation: rule => rule.required()
         },
     ],
 }
