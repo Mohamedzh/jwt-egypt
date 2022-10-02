bbfa91049ad9effe8c14c2f86a1aa81d6e843f61
MariamYoussef23 <mariamyoussef@aucegypt.edu>
2022-10-02 20:00:12 +0200

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -53,7 +53,7 @@ export async function getStaticProps() {
   )
 
   const vacancies = await getClient(false).fetch(
-    ` *[_type == "job"]{location, title, type, department->{title}, job_summary, responsibilities, qualifications}`
+    ` *[_type == "job"]{location, title, type, department->{title}, details}`
   )
 
   const stories = await getClient(false).fetch(
