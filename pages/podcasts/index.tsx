46dd527a297cc24cf14232b1e51d7775071fd5df
Mohamed Zahran <mohamed.zahran1@live.com>
2022-10-02 20:05:29 +0200

    podcast

---
+++ pages/podcasts/index.tsx
@@ -0,0 +1,86 @@
+import React from 'react'
+import Footer from '../../components/footerTab'
+import NavigationBar from '../../components/NavigationBar'
+import { getClient } from '../../lib/sanity'
+import { Howl, Howler } from 'howler';
+
+
+
+function Podcast({ data }) {
+    console.log(data.episodes)
+    console.log(data.videoEpisodes)
+
+    // let audio = new Audio(data.podcast[0].fileUrl)
+
+    // var sound = new Howl({
+    //     src: [`${data.podcast[0].fileUrl}`]
+    // });
+
+    // sound.play();
+    return (
+        <div
+        // className=" bg-cover h-screen bg-[url('/podcastBg.jpg')]"
+        >
+            <NavigationBar data={data} />
+            <div className='m-20'>
+                {data.videoEpisodes.map((video, idx) =>
+                    <div
+                        className='m-5'
+                        key={idx}
+                    >
+                        <p className='text-left my-5 text-3xl'>{video.title}</p>
+                        <video
+                            id="my-player"
+                            className="video-js h-96 aspect-video"
+                            controls
+                            preload="auto"
+                            poster=""
+                            data-setup='{}'>
+                            <source src={`${video.fileUrl}`}></source>
+                            {/* <source src="//vjs.zencdn.net/v/oceans.webm" type="video/webm"></source>
+                    <source src="//vjs.zencdn.net/v/oceans.ogv" type="video/ogg"></source> */}
+
+                        </video>
+                        <p className='text-left my-5 text-xl'>{video.description}</p>
+
+                    </div>
+                )}
+            </div>
+            {/* <button
+                onClick={() => sound.play()
+                }
+            >
+                Click to play
+            </button> */}
+            <Footer data={data} />
+        </div >
+    )
+}
+
+export default Podcast
+
+export async function getStaticProps() {
+
+    const quoteList = await getClient(false).fetch(
+        `*[_type == "quote"]{body, person->{department->{title}, name, "imageUrl":image.asset->url, job_title}, color-> {name, color_code}}`)
+
+    const themeColors = await getClient(false).fetch(
+        `*[_type == "siteTheme"]{firstColor->{color_code}, secondColor->{color_code}}`)
+
+    const JWTContact = await getClient(false).fetch(`*[_type == "contactUs"]`)
+    const navbarTheme = await getClient(false).fetch(
+        `*[_type == "navbarTheme"]{"logo":logo.asset->url, buttonText, logoTextColor->{color_code}, menuTextColor->{color_code}, altText}`
+    )
+
+    const podcast = await getClient(false).fetch(
+        `*[_type == "podcast"]{description, "fileUrl": audio.asset->url}`
+    )
+    const episodes = await getClient(false).fetch(
+        `*[_type == "episodes"]{title, "fileUrl": audio.asset->url}`
+    )
+    const videoEpisodes = await getClient(false).fetch(
+        `*[_type == "videoEpisodes"]{title, "fileUrl": video.asset->url, description}`
+    )
+
+    return { props: { data: { quoteList, themeColors, JWTContact, navbarTheme, podcast, episodes, videoEpisodes } } }
+}
